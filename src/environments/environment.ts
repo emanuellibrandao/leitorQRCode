ac412b00434ed026dd4e0353c2fbbaa843dadc9c
emanuellibrandao <emanuellibrandao@gmail.com>
2020-10-15 23:24:46 -0300

    Initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCCm_-tT2_my1AY8AC94H7s_f1LrZI-7Zo",
+    authDomain: "leitorqrcodefirebase-7a428.firebaseapp.com",
+    databaseURL: "https://leitorqrcodefirebase-7a428.firebaseio.com",
+    projectId: "leitorqrcodefirebase-7a428",
+    storageBucket: "leitorqrcodefirebase-7a428.appspot.com",
+    messagingSenderId: "608586704497",
+    appId: "1:608586704497:web:ea11cbbe8d2881a4df1dd0"
+ }
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
